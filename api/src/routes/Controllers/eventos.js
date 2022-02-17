d03c3ba89942bad4d8b790f558d60025869c804e
ViJo0803 <vijo967@gmail.com>
2022-02-17 16:42:39 -0500

    get post update

--- api/src/routes/Controllers/eventos.js
+++ api/src/routes/Controllers/eventos.js
@@ -1,10 +1,10 @@
-const { Usuario,Eventos} = require("../../db");
+const { Usuario, Eventos } = require("../../db");
 require("dotenv").config();
 
 
 async function createEventos(req, res) {
-  
-  const {    
+
+  const {
     nombre,
     tipo,
     fecha,
@@ -12,29 +12,40 @@ async function createEventos(req, res) {
     participantes,
   } = req.body;
 
- 
-  let participantesDb=participantes.filter((ele,pos)=>participantes.indexOf(ele)==pos)
-//
+  let participantesDb = participantes.filter((ele, pos) => participantes.indexOf(ele) == pos)
 
- let descripcionDb = await Usuario.findOne({
-    where: { nombre: participantesDb },
-  });
+  let i = 0;
 
-  if (descripcionDb!==null){
+  let contadorIngresados = participantes.length;
+  let contadorCreados = 0;
+
+  if (participantesDb.length !== 0) {
     let eventoCreado = await Eventos.create({
       nombre,
       tipo,
       fecha,
       hora,
     });
-    eventoCreado.addUsuario(descripcionDb);
-    res.send("Evento creado con exito")
+    participantesDb.forEach(async (element) => {
+      var descripcionDB = await Usuario.findOne({
+        where: { nombre: element },
+      });
+      if (descripcionDB) {
+        eventoCreado.addUsuario(descripcionDB);
+        console.log("Usuario creado!")
+        contadorCreados = contadorCreados+1
+      }
+    });
+    if (contadorIngresados !== await contadorCreados) {
+      res.send().status(200)
+    } else {
+      res.send().status(204)
+     }
   }
-  else {res.send("participante no existe").status(204)
-  //add viene de sequelize
-  //eventoCreado.addUsuario(participantesDb)
+  else {
+    res.send("participante no existe").status(204)
   }
-  
+
 }
 
 
@@ -42,14 +53,14 @@ async function createEventos(req, res) {
 async function getEvent(req, res, next) {
   try {
     const tipo = req.query.tipo;
-    if (tipo){
+    if (tipo) {
       let event = await Eventos.findOne({
         where: {
           tipo: tipo,
         },
       });
       event ? res.send(event).status(200) : res.send(null).status(204);
-    }else res.send(null);
+    } else res.send(null);
   } catch (error) {
     next(error);
   }
@@ -67,11 +78,11 @@ async function updateEvent(req, res, next) {
     fecha,
     participantes,
   } = req.body;
-  
-  let participantesDb=participantes.filter((ele,pos)=>participantes.indexOf(ele)==pos)
-//
 
- let descripcionDb = await Usuario.findOne({
+  let participantesDb = participantes.filter((ele, pos) => participantes.indexOf(ele) == pos)
+  //
+
+  let descripcionDb = await Usuario.findOne({
     where: { nombre: participantesDb },
   });
 
@@ -98,5 +109,5 @@ async function updateEvent(req, res, next) {
 }
 
 
-module.exports = {createEventos, getEvent,updateEvent}
+module.exports = { createEventos, getEvent, updateEvent }
 
